c8fa987460d5f424e05203cf4e4b957ec574b2f3
Eduardo Rodrigues <56962642+EduardoAFR@users.noreply.github.com>
2024-11-11 16:29:22 -0300

    Update relatorios.py

--- src/reports/relatorios.py
+++ src/reports/relatorios.py
@@ -3,25 +3,22 @@ from db_connection import conectar
 class Relatorio:
 
     @staticmethod
+
     def relatorio_pedidos_por_genero():
-        """Relatório sumarizando o valor total dos pedidos agrupados por gênero."""
-        connection = conectar()
-        cursor = connection.cursor()
-        
-        cursor.execute("""
-        SELECT g.nome_genero AS genero, SUM(p.quantidade) AS total_quantidade
-        FROM PEDIDOS p
-        JOIN LIVROS l ON p.id_livro = l.id_livro
-        JOIN GENEROS g ON l.id_genero = g.id_genero
-        GROUP BY g.nome_genero
-        """)
-        
-        resultados = cursor.fetchall()
-        print("\nRelatório de Vendas por Gênero:")
-        for genero, total_quantidade in resultados:
-            print(f"Gênero: {genero}, Total de Pedidos: {total_quantidade}")
-        
-        connection.close()
+     connection = conectar()
+     cursor = connection.cursor()
+
+     cursor.execute("""
+    SELECT g.nome_genero AS genero, SUM(p.quantidade) AS total_quantidade
+    FROM PEDIDOS p
+    JOIN LIVROS l ON p.id_livro = l.id_livro
+    JOIN GENEROS g ON l.id_genero = g.id_genero
+    GROUP BY g.nome_genero
+    """)
+    
+     resultados = cursor.fetchall()
+     connection.close()
+     return resultados
 
     @staticmethod
     def relatorio_pedidos_detalhados():
@@ -45,22 +42,20 @@ class Relatorio:
         connection.close()
 
     @staticmethod
-    def relatorio_pedidos_por_autor():
-        """Relatório sumarizando o valor total dos pedidos agrupados por Autor."""
+    def relatorio_todos_autores_com_pedidos():
         connection = conectar()
         cursor = connection.cursor()
         
+        # Usando LEFT JOIN para garantir que todos os autores sejam incluídos, mesmo que não tenham pedidos
         cursor.execute("""
-        SELECT autores.nome_autor, SUM(pedidos.quantidade) AS total_quantidade
-        FROM PEDIDOS 
-        INNER JOIN livros ON pedidos.id_livro = livros.id_livro
-        INNER JOIN autores ON livros.id_autor = autores.id_autor
-        GROUP BY autores.nome_autor
+            SELECT autores.nome_autor, 
+                   ISNULL(SUM(pedidos.quantidade), 0) AS total_quantidade
+            FROM autores
+            LEFT JOIN livros ON autores.id_autor = livros.id_autor
+            LEFT JOIN pedidos ON livros.id_livro = pedidos.id_livro
+            GROUP BY autores.nome_autor
         """)
         
         resultados = cursor.fetchall()
-        print("\nRelatório de Vendas por Autor:")
-        for nome_autor, total_quantidade in resultados:
-            print(f"Nome Autor: {nome_autor} Quantidade : {total_quantidade}")
-        
         connection.close()
+        return resultados
