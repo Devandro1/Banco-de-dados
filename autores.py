f2f26fe7831dad9171dd475be90ca0e6994254ab
Eduardo Rodrigues <56962642+EduardoAFR@users.noreply.github.com>
2024-11-11 15:04:22 -0300

    Update and rename autor.py to autores.py

--- autores.py
+++ autores.py
@@ -1,28 +1,28 @@
 from db_connection import conectar
 
 class Autor:
-    def __init__(self, id=None, nome=''):
-        self.id = id
-        self.nome = nome
+    def __init__(self, id_autor=None, nome_autor=''):
+        self.id = id_autor
+        self.nome = nome_autor
 
     def inserir(self):
         conn = conectar()
         cursor = conn.cursor()
-        cursor.execute("INSERT INTO autores (nome) VALUES (?)", (self.nome,))
+        cursor.execute("INSERT INTO autores (id_autor , nome_autor) VALUES (?,?)", (self.id,self.nome))
         conn.commit()
         conn.close()
 
     def atualizar(self):
         conn = conectar()
         cursor = conn.cursor()
-        cursor.execute("UPDATE autores SET nome = ? WHERE id = ?", (self.nome, self.id))
+        cursor.execute("UPDATE autores SET nome_autor = ? WHERE id_autor = ?", (self.nome, self.id))
         conn.commit()
         conn.close()
 
     def remover(self):
         conn = conectar()
         cursor = conn.cursor()
-        cursor.execute("DELETE FROM autores WHERE id = ?", (self.id,))
+        cursor.execute("DELETE FROM autores WHERE id_autor = ?", (self.id,))
         conn.commit()
         conn.close()
 
@@ -30,7 +30,7 @@ class Autor:
     def listar():
         conn = conectar()
         cursor = conn.cursor()
-        cursor.execute("SELECT * FROM autores")
+        cursor.execute("SELECT id_autor, nome_autor FROM autores")
         autores = cursor.fetchall()
         conn.close()
         return autores
