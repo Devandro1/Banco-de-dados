1f276b008751f159c34b306a1a587e91ac932874
Eduardo Rodrigues <56962642+EduardoAFR@users.noreply.github.com>
2024-11-11 15:03:44 -0300

    Update and rename livro.py to livros.py

--- livros.py
+++ livros.py
@@ -1,9 +1,9 @@
 from db_connection import conectar
 
 class Livro:
-    def __init__(self, id=None, titulo='', id_autor=None, id_genero=None):
-        self.id = id
-        self.titulo = titulo
+    def __init__(self, id_livro=None, titulo_livro='', id_autor=None, id_genero=None):
+        self.id = id_livro
+        self.titulo = titulo_livro
         self.id_autor = id_autor
         self.id_genero = id_genero
 
@@ -11,8 +11,8 @@ class Livro:
         conn = conectar()
         cursor = conn.cursor()
         cursor.execute(
-            "INSERT INTO livros (titulo, id_autor, id_genero) VALUES (?, ?, ?)",
-            (self.titulo, self.id_autor, self.id_genero)
+            "INSERT INTO livros (id_livro, titulo_livro, id_autor, id_genero) VALUES (?, ?, ?, ?)",
+            (self.id, self.titulo, self.id_autor, self.id_genero)
         )
         conn.commit()
         conn.close()
@@ -21,7 +21,7 @@ class Livro:
         conn = conectar()
         cursor = conn.cursor()
         cursor.execute(
-            "UPDATE livros SET titulo = ?, id_autor = ?, id_genero = ? WHERE id = ?",
+            "UPDATE livros SET titulo_livro = ?, id_autor = ?, id_genero = ? WHERE id_livro = ?",
             (self.titulo, self.id_autor, self.id_genero, self.id)
         )
         conn.commit()
@@ -30,7 +30,7 @@ class Livro:
     def remover(self):
         conn = conectar()
         cursor = conn.cursor()
-        cursor.execute("DELETE FROM livros WHERE id = ?", (self.id,))
+        cursor.execute("DELETE FROM livros WHERE id_livro = ?", (self.id,))
         conn.commit()
         conn.close()
 
@@ -38,7 +38,7 @@ class Livro:
     def listar():
         conn = conectar()
         cursor = conn.cursor()
-        cursor.execute("SELECT * FROM livros")
+        cursor.execute("SELECT livros.id_livro, livros.titulo_livro, generos.nome_genero, autores.nome_autor FROM livros INNER JOIN generos ON livros.id_genero = generos.id_genero INNER JOIN autores ON livros.id_autor = autores.id_autor ")
         livros = cursor.fetchall()
         conn.close()
         return livros
