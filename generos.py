f359ad05d26e3d50483a9ddb6c64cc3d74b6846f
Eduardo Rodrigues <56962642+EduardoAFR@users.noreply.github.com>
2024-11-11 15:04:03 -0300

    Update generos.py

--- generos.py
+++ generos.py
@@ -1,28 +1,14 @@
 from db_connection import conectar
 
 class Genero:
-    def __init__(self, id=None, nome=''):
-        self.id = id
-        self.nome = nome
+    def __init__(self, id_genero=None, nomegenero=''):
+        self.id = id_genero
+        self.nome = nomegenero
 
     def inserir(self):
         conn = conectar()
         cursor = conn.cursor()
-        cursor.execute("INSERT INTO generos (nome) VALUES (?)", (self.nome,))
-        conn.commit()
-        conn.close()
-
-    def atualizar(self):
-        conn = conectar()
-        cursor = conn.cursor()
-        cursor.execute("UPDATE generos SET nome = ? WHERE id = ?", (self.nome, self.id))
-        conn.commit()
-        conn.close()
-
-    def remover(self):
-        conn = conectar()
-        cursor = conn.cursor()
-        cursor.execute("DELETE FROM generos WHERE id = ?", (self.id,))
+        cursor.execute("INSERT INTO generos (id_genero, nome_genero) VALUES (?, ?)", (self.id, self.nome))
         conn.commit()
         conn.close()
 
@@ -30,7 +16,21 @@ class Genero:
     def listar():
         conn = conectar()
         cursor = conn.cursor()
-        cursor.execute("SELECT * FROM generos")
+        cursor.execute("SELECT id_genero, nome_genero FROM generos")
         generos = cursor.fetchall()
         conn.close()
         return generos
+
+    def atualizar(self):
+        conn = conectar()
+        cursor = conn.cursor()
+        cursor.execute("UPDATE generos SET nome_genero = ? WHERE id_genero = ?", (self.nome, self.id))
+        conn.commit()
+        conn.close()
+
+    def remover(self):
+        conn = conectar()
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM generos WHERE id_genero = ?", (self.id,))
+        conn.commit()
+        conn.close()
