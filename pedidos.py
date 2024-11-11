9f5aa3d77722847c0e21f5d39a93e692fc9519ce
Eduardo Rodrigues <56962642+EduardoAFR@users.noreply.github.com>
2024-11-11 15:02:55 -0300

    Update and rename pedido.py to pedidos.py

--- pedidos.py
+++ pedidos.py
@@ -1,17 +1,18 @@
 from db_connection import conectar
 
 class Pedido:
-    def __init__(self, id=None, id_livro=None, quantidade=0):
-        self.id = id
+    def __init__(self, id_pedido=None, id_livro=None, quantidade=0,data_pedido=None):
+        self.id = id_pedido
         self.id_livro = id_livro
         self.quantidade = quantidade
+        self.data_pedido = data_pedido
 
     def inserir(self):
         conn = conectar()
         cursor = conn.cursor()
         cursor.execute(
-            "INSERT INTO pedidos (id_livro, quantidade) VALUES (?, ?)",
-            (self.id_livro, self.quantidade)
+            "INSERT INTO pedidos (id_pedido, id_livro, quantidade,data_pedido) VALUES (?, ?, ?,?)",
+            (self.id,self.id_livro, self.quantidade, self.data_pedido)
         )
         conn.commit()
         conn.close()
@@ -20,8 +21,8 @@ class Pedido:
         conn = conectar()
         cursor = conn.cursor()
         cursor.execute(
-            "UPDATE pedidos SET id_livro = ?, quantidade = ? WHERE id = ?",
-            (self.id_livro, self.quantidade, self.id)
+            "UPDATE pedidos SET id_livro = ?, quantidade = ?, data_pedido = ? WHERE id_pedido = ?",
+            (self.id_livro, self.quantidade, self.data_pedido, self.id)
         )
         conn.commit()
         conn.close()
@@ -29,7 +30,7 @@ class Pedido:
     def remover(self):
         conn = conectar()
         cursor = conn.cursor()
-        cursor.execute("DELETE FROM pedidos WHERE id = ?", (self.id,))
+        cursor.execute("DELETE FROM pedidos WHERE id_pedido = ?", (self.id,))
         conn.commit()
         conn.close()
 
@@ -37,7 +38,7 @@ class Pedido:
     def listar():
         conn = conectar()
         cursor = conn.cursor()
-        cursor.execute("SELECT * FROM pedidos")
+        cursor.execute("SELECT pedidos.id_pedido, livros.titulo_livro, pedidos.quantidade , pedidos.data_pedido  FROM pedidos INNER JOIN livros ON livros.id_livro = livros.id_livro")
         pedidos = cursor.fetchall()
         conn.close()
         return pedidos
